746ac385a133831374d04b067f0e33cd5c3b6dd8
sbx_user1051 <sbx_user1051@169.254.16.21>
2020-02-09 17:09:27 +0000

    Sun Feb 09 17:09:27 UTC 2020

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using BeFaster.Runner.Exceptions;
 
 namespace BeFaster.App.Solutions.CHK
@@ -12,8 +13,14 @@ namespace BeFaster.App.Solutions.CHK
             var basketTotal = 0;
             for (var skuCount = 0; skuCount < skus.Length; skuCount++)
             {
-                var currentSku = 
+                var currentSku = store.ProductsInStore.FirstOrDefault(p => p.Sku == skus[skuCount]);
+                if (currentSku == null)
+                {
+                    basketTotal = -1;
+                    break;
+                }
 
+                basketTotal += currentSku.Price;
             }
 
             return basketTotal;
@@ -28,23 +35,24 @@ namespace BeFaster.App.Solutions.CHK
         {
             ProductsInStore = new List<Product>()
             {
-                new Product("A", 50),
-                new Product("B", 30),
-                new Product("C", 20),
-                new Product("D", 15)
+                new Product('A', 50),
+                new Product('B', 30),
+                new Product('C', 20),
+                new Product('D', 15)
             };
         }
     }
 
     public class Product
     {
-        public string Sku { get; private set; }
+        public char Sku { get; private set; }
         public int Price { get; private set; }
 
-        public Product(string sku, int price)
+        public Product(char sku, int price)
         {
             this.Sku = sku;
             this.Price = price;
         }
     }
 }
+

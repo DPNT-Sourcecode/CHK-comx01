f5289d1d95ce0e20ce7f8947708609b1e3d85c76
sbx_user1051 <sbx_user1051@169.254.16.21>
2020-02-09 17:54:27 +0000

    Sun Feb 09 17:54:27 UTC 2020

---
+++ src/BeFaster.App.Tests/Solutions/CHK/StoreTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BeFaster.App.Solutions.CHK;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BeFaster.App.Tests.Solutions.CHK
+{
+    [TestFixture]
+    public class StoreTests
+    {
+        [Test]
+        public void ShouldHave4AllowedProducts()
+        {
+            //Arrange
+            var store = new Store();
+            //Act
+
+            //Assert
+            store.ProductsInStore.Count().Should().Be(4);
+        }
+
+    }
+}

3da9fa556080fde84f3c89fdd484829e3b98f0c9
sbx_user1051 <sbx_user1051@169.254.16.21>
2020-02-09 18:09:27 +0000

    Sun Feb 09 18:09:27 UTC 2020

--- src/BeFaster.App.Tests/Solutions/CHK/ProductTests.cs
+++ src/BeFaster.App.Tests/Solutions/CHK/ProductTests.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BeFaster.App.Solutions.CHK;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace BeFaster.App.Tests.Solutions.CHK
 {
-    class ProductTests
+    [TestFixture]
+    public class ProductTests
     {
+        [Test]
+        public void ShouldSetupCorrect()
+        {
+            //Arrange
+            var product = new Product('A', 10, null);
+            //Act
+            //Assert
+            product.Sku.Should().Be('A');
+            product.Price.Should().Be(10);
+            product.Offer.Should().BeNull();
+        }
+
+        [Test]
+        public void ShouldCalculateCorrectPriceWithoutOffer()
+        {
+            //Arrange
+            var product = new Product('A', 10, null);
+            //Act
+            var totalPrice = product.GetPrice("AAA");
+            //Assert
+            totalPrice.Should().Be(30);
+        }
     }
 }
+

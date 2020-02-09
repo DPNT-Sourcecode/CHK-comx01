d9f55d3623a32cec690af09a0a837bd4d3e52b34
sbx_user1051 <sbx_user1051@169.254.16.21>
2020-02-09 16:42:27 +0000

    Sun Feb 09 16:42:27 UTC 2020

--- src/BeFaster.App.Tests/Solutions/CHK/CheckoutTests.cs
+++ src/BeFaster.App.Tests/Solutions/CHK/CheckoutTests.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BeFaster.App.Solutions.CHK;
+using NUnit.Framework;
 
 namespace BeFaster.App.Tests.Solutions.CHK
 {
-    class CheckoutTests
+    [TestFixture]
+    public class CheckoutTests
     {
+        [TestCase("A", ExpectedResult = 50)]
+        [TestCase("B", ExpectedResult = 30)]
+        [TestCase("C", ExpectedResult = 20)]
+        [TestCase("D", ExpectedResult = 15)]
+        public int CartTotal(string cartItems)
+        {
+            return CheckoutSolution.ComputePrice(cartItems);
+        }
+
     }
 }
+

71adfa1fa1910b539ce7541e3272cf2cafab48b5
sbx_user1051 <sbx_user1051@169.254.16.21>
2020-02-09 16:25:50 +0000

    Sun Feb 09 16:25:50 UTC 2020

--- src/BeFaster.App.Tests/Solutions/HLO/HelloWorldTests.cs
+++ src/BeFaster.App.Tests/Solutions/HLO/HelloWorldTests.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BeFaster.App.Solutions.HLO;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace BeFaster.App.Tests.Solutions.HLO
 {
+    [TestFixture]
     class HelloWorldTests
     {
+        [Test]
+        public void ReturnStringTest()
+        {
+            HelloSolution.Hello("").Should().Be("Hello World!");
+
+        }
+
     }
 }
